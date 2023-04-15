f84531109ef5118f73e43b9e01ba650556cffeb9
PouyaniArmin <pouyaniarmin@gmail.com>
2023-04-15 09:50:27 +0330

    link home

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -31,7 +31,7 @@
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/product">Procduct</a>
