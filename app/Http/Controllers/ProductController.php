3f342ceb0c7fdfb330b73056c7646f7d832705a2
PouyaniArmin <pouyaniarmin@gmail.com>
2023-04-17 10:43:35 +0330

    call path images table

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,8 +7,9 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(Request $request){
-        $products=Product::all();
-        return view('product',compact('products'));
+    public function index(Request $request)
+    {
+        $products = Product::with('pictures')->get();
+        return view('product', compact('products'));
     }
 }
