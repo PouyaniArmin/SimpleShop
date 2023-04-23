5c86f79c4ac37eb5883d5002b9e274f139b87bc1
PouyaniArmin <pouyaniarmin@gmail.com>
2023-04-23 16:19:26 +0330

    javaScript file

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+const imgs=document.querySelectorAll('img-select a');
+const imgBtns=[...imgs];
+let imgId=1;
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click',(event)=>{
+        event.preventDefault();
+        imgId=imgItem.dataset.id;
+        slidImage();
+    });
+});
+
+function slidImage() {
+        const displayWidth=document.querySelector('.img-showcase img:first-child').clientWidth;
+        document.querySelector('.img-showcase').style.transform=`translateX(${- (imgId - 1) * displayWidth}px)`;
+}
