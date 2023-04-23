0771661e72edb3dac8b1836e6f39803741939530
PouyaniArmin <pouyaniarmin@gmail.com>
2023-04-23 16:21:33 +0330

    view product detail

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -64,7 +64,7 @@
                                 <h5 class="card-title">{{$data->title}}</h5>
                                 <p class="card-text text-truncate">{{$data->description}}</p>
                                 <p class="card-text text-truncate">{{$data->price}} $</p>
-                                <a class="icon-link icon-link-hover" href="#">
+                                <a class="icon-link icon-link-hover" href="{{route('product.info',$data->id)}}">
                                     Read More
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
--- resources/views/singelProduct.blade.php
+++ resources/views/singelProduct.blade.php
@@ -2,52 +2,50 @@
 @section('content')
 <!-- product list-->
 <div class="py-4" style="max-width:98%;">
-    <div class="container pt-4 py-4">
-        <div class="row">
-            <div class="col-6">
-                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner single-product">
-                        <div class="carousel-item active" data-bs-interval="10000">
-                            <img src="{{asset('images/02.jpg')}}" class="d-block" alt="...">
-                        </div>
-                        <div class="carousel-item" data-bs-interval="2000">
-                            <img src="{{asset('images/01.jpeg')}}" class="d-block" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="{{asset('images/lap.png')}}" class="d-block" alt="...">
-                        </div>
+    <div class="container sproduct my-5 pt-5">
+        <div class="row mt-5">
+            <div class="main col-lg-5 col-md-12 col-sm-12">
+                <!-- big image -->
+                <img src="{{asset($product['pictures'][0]['path'])}}" class="big img-fluid w-100 border" id="MainImg" alt="">
+
+                <div class="small-img-group pt-2">
+                    <!-- small image -->
+                    @foreach($product->pictures as $pic)
+                    <div class="small-img-col" id="slider">
+                        <img src="{{asset($pic->path)}}" width="100%" onclick="this.src='path'" class="each small-img active" alt="">
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden" style="background-color:green;">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
+                    @endforeach
+
                 </div>
             </div>
-            <!-- description -->
-            <div class="col-6">
-                <!-- name -->
-                <div class="py-2">
-                    <h1 class="fs-2">Product Name</h1>
-                </div>
-                <!-- body -->
-                <div class="text-muted">
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available</p>
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available</p>
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available</p>
-                </div>
-                <!-- buutom -->
-                <div class="row py-2 text-center">
-                    <div class="col-4">
-                        <a href="#" class="btn btn-danger">Add To Cart</a>
-                    </div>
-                    <div class="col-5"><a href="" class="btn btn-primary"> Countinue Shopping</a></div>
+
+            <div class="col-lg-6 col-md-12 col-sm-12">
+                <h3 class="my-4">{{$product->title}}</h3>
+
+                <h4 class="mt-5">Product Details</h4>
+                <span>{{$product->description}}</span>
+                <div class="py-4">
+                    <input type="number" value="1">
+                    <button class="btn btn-dark">Add To Cart</button>
+
                 </div>
             </div>
+
+
         </div>
     </div>
 </div>
+<script>
+    var main = document.querySelector(".main"),
+        big = main.querySelector(".big"),
+        smalls = main.querySelectorAll(".each")
+    big.setAttribute("src", smalls[0].getAttribute("src"))
+    smalls.forEach(each => {
+        each.onclick = function() {
+            smalls.forEach(ss => ss.classList.remove("active"))
+            each.classList.add("active")
+            big.setAttribute("src", each.getAttribute("src"))
+        }
+    });
+</script>
 @stop
